7dce99fb320385624d718b8ae6c5bf7157b7a1c3
simorai <109289730+simorai@users.noreply.github.com>
2025-01-19 20:05:53 +0000

    Add project files.

---
+++ ExerciciosFiles/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+
+namespace ExerciciosFiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootPath = @"C:\Projetos\ExerciciosFiles\ExerciciosFiles\TestFiles\";
+
+            string[] dirs = Directory.GetDirectories(rootPath, "*.*", SearchOption.AllDirectories);
+
+            foreach (string dir in dirs)
+            {
+                Console.WriteLine(dir);
+            }
+
+
+        }
+    //TODO validaçoes
+    
+        public static void SearchDirectory(string path)
+        {
+            string[] dirs = Directory.GetDirectories(path, "*.*", SearchOption.AllDirectories);
+            foreach (string dir in dirs)
+            {
+                Console.WriteLine(dir);
+            }
+        }
+
+    }
+}
