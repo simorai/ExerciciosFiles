495f81b3b723984e2d94f0ad95c5dc3e2aaa3f4a
simorai <109289730+simorai@users.noreply.github.com>
2025-01-19 21:43:33 +0000

    file path exercises

---
+++ ExerciciosFiles/Methods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosFiles
+{
+    internal class Methods
+    {
+        //TODO validaçoes para nao crashar quando se tenta aceder a uma pasta que nao existe.    
+        public static void SearchDirectory(string path)
+        {
+            string[] dirs = Directory.GetDirectories(path, "*.*", SearchOption.AllDirectories);
+            foreach (string dir in dirs)
+            {
+                Console.WriteLine(dir);
+            }
+        }
+    }
+}
--- ExerciciosFiles/Program.cs
+++ ExerciciosFiles/Program.cs
@@ -6,27 +6,79 @@ namespace ExerciciosFiles
     {
         static void Main(string[] args)
         {
+            //Aceder a pastas
             string rootPath = @"C:\Projetos\ExerciciosFiles\ExerciciosFiles\TestFiles\";
 
-            string[] dirs = Directory.GetDirectories(rootPath, "*.*", SearchOption.AllDirectories);
+            //string[] dirs = Directory.GetDirectories(rootPath, "*.*", SearchOption.AllDirectories);
 
-            foreach (string dir in dirs)
+            //foreach (string dir in dirs)
+            //{
+            //    Console.WriteLine(dir);
+            //}
+
+            //aceder a ficheiros
+            //var files = Directory.GetFiles(rootPath, "*.*", SearchOption.AllDirectories);
+
+            //foreach (string file in files)
+            //{
+            //    Console.WriteLine(file);
+            //    Console.WriteLine(Path.GetFileName(file));
+            //    Console.WriteLine(Path.GetFileNameWithoutExtension(file));
+            //    Console.WriteLine(Path.GetFullPath(file));
+            //    Console.WriteLine(Path.GetDirectoryName(file));
+            //    var info = new FileInfo(file);
+            //    Console.WriteLine($" {Path.GetFileName(file)}: {info.Length} bytes ");
+            //}
+
+            //string newPath = @"C:\Projetos\ExerciciosFiles\ExerciciosFiles\TestFiless\";
+            ////Directory.CreateDirectory(newPath); //Cria a nova diretoria sem perguntar
+
+            //bool directoryExists =  Directory.Exists(@"C:\Projetos\ExerciciosFiles\ExerciciosFiles\TestFiless\");
+
+            //if (directoryExists)
+            //{
+            //    Console.WriteLine("Directory Exists");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Directory does not exist");
+            //    Directory.CreateDirectory(newPath);
+            //}
+
+            //-------------------------------
+
+            //string[] files = Directory.GetFiles(rootPath);
+            //string destinationFolder = @"C:\Projetos\ExerciciosFiles\ExerciciosFiles\TestFiles\SubFolderA";
+
+            //foreach (string file in files)
+            //{
+            //    File.Copy(file, $"{destinationFolder} {Path.GetFileName(file)}", true);
+            //    Console.WriteLine(file);
+            //}
+
+            //for (int i = 0; i < files.Length; i++)
+            //{
+            //    File.Copy(files[i], $"{destinationFolder}{i}.txt", true);
+            //}
+
+            //foreach (string file in files)
+            //{
+            //    File.Move(file, $"{destinationFolder} {Path.GetFileName(file)}");
+            //    Console.WriteLine(file);
+            //}
+
+            var files = Directory.GetFiles(rootPath, "*folder*.*", SearchOption.AllDirectories);
+
+            foreach (string file in files)
             {
-                Console.WriteLine(dir);
-            }
-
-
-        }
-    //TODO validaçoes
-    
-        public static void SearchDirectory(string path)
-        {
-            string[] dirs = Directory.GetDirectories(path, "*.*", SearchOption.AllDirectories);
-            foreach (string dir in dirs)
-            {
-                Console.WriteLine(dir);
+                Console.WriteLine(file);
+                //Console.WriteLine(Path.GetFileName(file));
+                //Console.WriteLine(Path.GetFileNameWithoutExtension(file));
+                //Console.WriteLine(Path.GetFullPath(file));
+                //Console.WriteLine(Path.GetDirectoryName(file));
+                //var info = new FileInfo(file);
+                //Console.WriteLine($" {Path.GetFileName(file)}: {info.Length} bytes ");
             }
         }
-
     }
 }
